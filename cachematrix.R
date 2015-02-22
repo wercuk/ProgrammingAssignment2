0f973405fd4c6bafa044739efbd18daf2e4878cb
wercuk <wercuk@gmail.com>
2015-02-22 17:53:13 +0200

    Main Change. Working R code

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,17 @@
 ## It's something about matrix
-## functions do
 
 ## Cache and check function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        inv <- x$getsolve()
+        if(!is.null(inv)) {
+                message("getting cached data")
+                return(inv)
+        }
+        data <- x$get()
+        inv <- solve(data)
+        x$setsolve(inv)
+        inv        
 }
 
 
@@ -12,4 +19,15 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        inv <- NULL
+        set <- function(y) {
+                x <<- y
+                inv <<- NULL
+        }
+        get <- function() x
+        setsolve <- function(solve) inv <<- solve
+        getsolve <- function() inv
+        list(set = set, get = get,
+             setsolve = setsolve,
+             getsolve = getsolve)
 }
